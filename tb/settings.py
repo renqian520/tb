fab1ba64cedb2339507e2fa9add0f7b5d84968e4
renqian520 <35051100+renqian520@users.noreply.github.com>
2018-02-27 15:30:26 +0800

    Update settings.py

--- tb/settings.py
+++ tb/settings.py
@@ -72,7 +72,7 @@ RANDOMIZE_DOWNLOAD_DELAY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
